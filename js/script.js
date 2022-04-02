171d47e9d3923c88617cf57262b121d7c0c588f0
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-04-01 23:09:13 -0300

    adiciona tela de finalizacao e integracao com whatsapp

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 let numOrders = 0;
+let plate;
+let platePrice;
+let drink;
+let drinkPrice;
+let dessert;
+let dessertPrice;
+let fullPrice;
+let textWhatsapp;
 
 function verifyThreeOrders(numOrders) {
     const makeOrderBtn = document.querySelector(".bottom-bar button:first-child");
 
     if(numOrders === 3) {
         document.querySelector(".bottom-bar button:first-child").classList.add("not-visible");
-        document.querySelector(".bottom-bar button:last-child").classList.add("visible");
+        document.querySelector(".bottom-bar button:last-child").classList.remove("not-visible");
     } else if (makeOrderBtn.classList.contains("not-visible")) {
-        document.querySelector(".bottom-bar button:last-child").classList.remove("visible");
+        document.querySelector(".bottom-bar button:last-child").classList.add("not-visible");
         document.querySelector(".bottom-bar button:first-child").classList.remove("not-visible");
     }
 }
@@ -18,7 +26,6 @@ function selectedItem(element) {
     
     if(alredySelected !== null) {
         alredySelected.classList.remove("selected-border");
-        alredySelected.querySelector("ion-icon").classList.remove("visible");
         alredySelected.querySelector("ion-icon").classList.add("not-visible");
 
         numOrders--;
@@ -26,13 +33,57 @@ function selectedItem(element) {
     
     if(alredySelected !== element) {
         element.classList.add("selected-border");
-        
-        const icon = element.querySelector("ion-icon");
-        icon.classList.add("visible");
-        icon.classList.remove("not-visible");
-        
+        element.querySelector("ion-icon").classList.remove("not-visible");
+        setOrder(element, father);
+
         numOrders++;
     }
 
     verifyThreeOrders(numOrders);
+}
+
+function setOrder(element, elementFather) {
+    const titleType = elementFather.parentNode.querySelector("h2").innerHTML;
+    
+    if(titleType.includes("prato")) {
+        plate = element.querySelector("h4").innerHTML;
+        platePrice = element.querySelector(".food-option > div > h4").innerHTML;
+
+    } else if(titleType.includes("bebida")) {
+        drink = element.querySelector("h4").innerHTML;
+        drinkPrice = element.querySelector(".food-option > div > h4").innerHTML;
+
+    } else if(titleType.includes("sobremesa")) {
+        dessert = element.querySelector("h4").innerHTML;
+        dessertPrice = element.querySelector(".food-option > div > h4").innerHTML;
+
+    }
+}
+
+function showConfirmView() {
+    document.querySelector(".confirm-view").classList.remove("not-visible");
+
+    fullPrice = (Number(platePrice.replace(/\D/g,'')) + Number(drinkPrice.replace(/\D/g,'')) + Number(dessertPrice.replace(/\D/g,''))) / 100;
+    fullPrice = fullPrice.toFixed(2).toString();
+
+    document.querySelector(".food-info > p:first-child").innerHTML = plate;
+    document.querySelector(".food-info > p:last-child").innerHTML = platePrice;
+    document.querySelector(".food-info:nth-child(3) > p:first-child").innerHTML = drink;
+    document.querySelector(".food-info:nth-child(3) > p:last-child").innerHTML = drinkPrice;
+    document.querySelector(".food-info:nth-child(4) p:first-child").innerHTML = dessert;
+    document.querySelector(".food-info:nth-child(4) p:last-child").innerHTML = dessertPrice;
+    document.querySelector(".food-info:nth-child(5) p:last-child").innerHTML = `R$ ${fullPrice.replace(".", ",")}`;
+}
+
+function cancelOrder() {
+    document.querySelector(".confirm-view").classList.add("not-visible");
+}
+
+function confirmOrder() {
+    textWhatsapp = `Olá, gostaria de fazer o pedido:
+- Prato: ${plate}
+- Bebida: ${drink}
+- Sobremesa: ${dessert}
+Total: R$ ${fullPrice.replace(".", ",")}`;
+    window.open(`https://wa.me/5561994344875?text=${encodeURIComponent(textWhatsapp)}`);
 }
